d4ade3a4782fa58ded7c7357737d19f7aad3d3fc
Guilherme Augusto  Santos <guipratiko@MacBook-Pro-de-Guilherme.local>
2025-09-04 14:17:55 -0300

    fix: define porta padrão como 85

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require('path');
 const { google } = require('googleapis');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 85;
 
 // Middleware
 app.use(cors());
